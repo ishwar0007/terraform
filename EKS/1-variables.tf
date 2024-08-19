4c1e8e98754603706b2b5f4406bf49f6256792ac
Ishwar Chand Saini <89911623+ishwar0007@users.noreply.github.com>
2024-08-19 14:23:28 +0530

    Update 1-variables.tf

--- EKS/1-variables.tf
+++ EKS/1-variables.tf
@@ -13,6 +13,11 @@ variable "AWS-Secret-Key" {
 variable "EKS-Name" {
   default = "Hunter-EKS"
 }
+
+variable "EKS-Version" {
+  default = "1.30"
+}
+
 output "combined_output_list" {
   value = ["AWS-Region = ${var.AWS-Region}", "AWS-Access-Key = ${var.AWS-Access-Key}", "AWS-Secret-Key = ${var.AWS-Secret-Key}", "EKS-Name = ${var.EKS-Name}"]
 }
