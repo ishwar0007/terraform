ee7d2b0c50685100c8552b06a34089b3870eea1b
Ishwar Chand Saini <89911623+ishwar0007@users.noreply.github.com>
2024-08-19 16:23:32 +0530

    Update 4-helm.tf

--- EKS-with-ALB-with-Default-VPC/4-helm.tf
+++ EKS-with-ALB-with-Default-VPC/4-helm.tf
@@ -4,7 +4,7 @@ provider "helm" {
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", module.eks.EKS-Name]
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
       command     = "aws"
     }
   }
